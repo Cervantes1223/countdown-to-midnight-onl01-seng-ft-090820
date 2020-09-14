ae6392a9b11f5fc0a0c3ec1a12f4a6174ded9e94
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-14 17:43:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  while number > 0
+   puts "#{number} SECOND(S)!"
+   number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+def countdown_with_slee (sleep )
+  sleep 5 seconds 
 end
